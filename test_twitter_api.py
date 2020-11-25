baa669c5427ca28237937f28a46cbce6863f85f3
ThibautHurson <thibaut.hurson@supelec.fr>
2020-11-25 12:07:40 +0100

    Récupération de Tweet: OK

--- test_twitter_api.py
+++ test_twitter_api.py
@@ -1,17 +1,26 @@
 import requests
-import os
+import pandas as pd
 import json
+import ast
+import yaml
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 
+def process_yaml():
+    with open("config.yaml") as file:
+        return yaml.safe_load(file)
 
-def auth():
-    return os.environ.get("BEARER_TOKEN")
+def create_bearer_token(data):
+    return data["search_tweets_api"]["bearer_token"]
 
+def twitter_auth_and_connect(bearer_token, url):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
 
-def create_url():
-    query = "from:twitterdev -is:retweet"
+def create_twitter_url():
+
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -19,32 +28,23 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
-    )
+    handle = "PSG_English"
+    max_results = 10
+    mrf = "max_results={}".format(max_results)
+    q = "query=from:{}".format(handle)
+    #tweet_fields = "tweet.fields=author_id"
+    url = "https://api.twitter.com/2/tweets/search/recent?{}&{}".format(
+       mrf, q)# tweet_fields
     return url
 
 
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-
-def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
-
-
 def main():
-    bearer_token = auth()
-    url = create_url()
-    headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    url = create_twitter_url()
+    data = process_yaml()
+    bearer_token = create_bearer_token(data)
+    res_json = twitter_auth_and_connect(bearer_token, url)
+    print(json.dumps(res_json, indent=4, sort_keys=True))
+
 
 
 if __name__ == "__main__":
