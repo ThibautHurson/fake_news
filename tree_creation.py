1ed6b65914524c48bad31b92786fb3120776ce30
ThibautHurson <thibaut.hurson@supelec.fr>
2020-12-09 09:06:08 +0100

    structure de l arbre: ok

--- tree_creation.py
+++ tree_creation.py
@@ -1,7 +1,39 @@
 import pandas as pd
 import numpy as np
 
+import twint
+
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data, children=None):
         self.data = data
-        self.children = []
+        if children: self.children = children
+        else: self.children = []
+
+
+    def add_children(self,child):
+        assert(child)
+        self.children.append(child)
+
+
+root = Node(1)
+root.add_children(None)
+print(root.data)
+print(root.children)
+
+# # Configure
+# c = twint.Config()
+# c.Username = "realDonaldTrump"
+# c.Search = "great"
+
+# # Run
+# twint.run.Search(c)
+
+# users = [
+#     'shakira',
+#     'KimKardashian',
+#     'rihanna',
+#     'jtimberlake',
+#     'KingJames',
+#     'neymarjr',
+# ]
