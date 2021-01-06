ba24095f5b1a91e5b9edd9d2f96df9f63d047b1d
ThibautHurson <thibaut.hurson@supelec.fr>
2021-01-06 19:03:03 +0100

    fonction daffichage neato à tester

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 from find_parents import *
 from create_tree import *
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def main() :
-    db_tweet = pd.read_csv('twint_ForumCarbone.csv',encoding='latin-1')
-    db_retweet = pd.read_csv('twint_RTTest.csv',encoding='latin-1')
-    db = pd.concat([db_tweet,db_retweet],ignore_index=True)
+    db_tweet = pd.read_csv('twint_ForumCarbone.csv', encoding='latin-1')
+    db_retweet = pd.read_csv('twint_RTTest.csv', encoding='latin-1')
+    db = pd.concat([db_tweet,db_retweet], ignore_index=True)
     parents,enfants = find_parents(db)
     db['Parents'] = parents
     db['Enfants'] = enfants
@@ -15,11 +16,26 @@ def main() :
     # print(db[db.Parents==-1].head())
     # print(db[db.Parents==0].head())
 
-    # print(db.columns)
-    graph = create_tree(db)
-    p = nx.drawing.nx_pydot.to_pydot(graph)
-    p.write_png('prapagation_graph.png')
+    # print(list(db['Enfants']))
 
+    graph = create_tree(db)
+    # subgraph = graph.subgraph([0,1,2,3,4])
+    # p = nx.drawing.nx_pydot.to_pydot(subgraph)
+    # p.write_png('prapagation_graph.png')
+    
+    # nx.draw_networkx(graph)
+    # plt.show()
+
+    # p = nx.drawing.nx_pydot.to_pydot(graph)
+    # p.write_png('prapagation_graph.png')
+
+    pos = nx.nx_pydot.graphviz_layout(graph, prog="neato")
+    pos[0] = np.array([0, 0])
+    nx.draw_shell(graph,alpha=0.4,pos=pos)
+    plt.show()
     return
 
-main()
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
