e07c9b7ec47729c81863e4bbf7f074091161a085
ThibautHurson <thibaut.hurson@supelec.fr>
2021-01-13 09:32:41 +0100

    plot subgraph successfull

--- create_tree.py
+++ create_tree.py
@@ -3,6 +3,7 @@ import numpy as np
 # from graphviz import Graph
 import networkx as nx
 import matplotlib.pyplot as plt
+# import igraph as ig
 
 #Tweet id c'est l'id du tweet
 #Tweet parent c'est l'id de son parent (si c'est un RT)
@@ -10,29 +11,37 @@ import matplotlib.pyplot as plt
 
 
 #We are considering tweet_id, tweet_parent, tweet_fils to create the tree
-def create_tree(df):
+def create_tree(df, root_child = None):
     def helper(G, twt_id, df):
         childs = df[df['id'] == twt_id]['Enfants'].values[0]
         if not childs:
             return
         for child in childs:
-            if child != twt_id:
+            if child != int(float(twt_id)):
                 G.append((twt_id, child))
                 helper(G, child, df)
 
     G = []
+    df_roots = df[(df['Parents'] == 0)]# | (df['Parents'] == -1)]  #when 0, the tweet corresponds to an original tweet
+    if root_child:
+        for twt_id in df_roots['id'][:root_child]:
+            G.append((0, twt_id))
+            helper(G, twt_id, df)        
+    else:
+        for twt_id in df_roots['id']:
+            G.append((0, twt_id))
+            helper(G, twt_id, df)
 
-    df_roots = df[df['Parents'] == 0]# or df['Parents'].values[0] == -1]  #when 0, the tweet corresponds to an original tweet
-    for twt_id in df_roots['id']:
-        G.append((0, twt_id))
-        helper(G, twt_id, df)
     return nx.DiGraph(G)
 
 
-
-# df = pd.DataFrame([[1,-1,[5,7]],[2,-1,[3]],[3,2,[4]],[4,3,[]],[5,1,[6]],[6,5,[]],[7,1,[8,9]],[8,7,[10]],[9,7,[]],[10,8,[]]],index=[1,2,3,4,5,6,7,8, 9, 10], columns=['tweet_id', 'tweet_parent', 'tweet_fils'])
+#Pour faire marcher l'exemple ci-dessous, décommenter | (df['Parents'] == -1)]
+# df = pd.DataFrame([[1,-1,[5,7]],[2,-1,[3]],[3,2,[4]],[4,3,[]],[5,1,[6]],[6,5,[]],[7,1,[8,9]],[8,7,[10]],[9,7,[]],[10,8,[]]],index=[1,2,3,4,5,6,7,8, 9, 10], columns=['id', 'Parents', 'Enfants'])
 # graph = create_tree(df)
 # p = nx.drawing.nx_pydot.to_pydot(graph)
 # p.write_png('prapagation_graph.png')
-# # nx.draw(p, with_labels=True, font_weight='bold')
-# # plt.show()
+# nx.draw(p, with_labels=True, font_weight='bold')
+# plt.show()
+
+
+# def create_subtree(db)
--- main.py
+++ main.py
@@ -4,38 +4,44 @@ from find_parents import *
 from create_tree import *
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot  import graphviz_layout
+# import igraph as ig
 
-def main() :
-    db_tweet = pd.read_csv('twint_ForumCarbone.csv', encoding='latin-1')
-    db_retweet = pd.read_csv('twint_RTTest.csv', encoding='latin-1')
-    db = pd.concat([db_tweet,db_retweet], ignore_index=True)
-    parents,enfants = find_parents(db)
-    db['Parents'] = parents
-    db['Enfants'] = enfants
-    # print(db[db.Parents==-1].shape)
-    # print(db[db.Parents==-1].head())
-    # print(db[db.Parents==0].head())
+# def main() :
+db_tweet = pd.read_csv('twint_ForumCarbone.csv', encoding='latin-1')
+db_retweet = pd.read_csv('twint_RTTest.csv', encoding='latin-1')
+db = pd.concat([db_tweet,db_retweet], ignore_index=True)
+parents,enfants = find_parents(db)
+db['Parents'] = parents
+db['Enfants'] = enfants
+# print(db[db.Parents==-1].shape)
+# print(db[db.Parents==-1].head())
+# print(db[db.Parents==0].head())
 
-    # print(list(db['Enfants']))
+# print(list(db['Enfants']))
 
-    graph = create_tree(db)
-    # subgraph = graph.subgraph([0,1,2,3,4])
-    # p = nx.drawing.nx_pydot.to_pydot(subgraph)
-    # p.write_png('prapagation_graph.png')
-    
-    # nx.draw_networkx(graph)
-    # plt.show()
+# subgraph = graph.subgraph([0,1,2,3,4])
+# p = nx.drawing.nx_pydot.to_pydot(subgraph)
+# p.write_png('prapagation_graph.png')
 
-    # p = nx.drawing.nx_pydot.to_pydot(graph)
-    # p.write_png('prapagation_graph.png')
+# nx.draw(graph)
+# plt.show()
 
-    pos = nx.nx_pydot.graphviz_layout(graph, prog="neato")
-    pos[0] = np.array([0, 0])
-    nx.draw_shell(graph,alpha=0.4,pos=pos)
-    plt.show()
-    return
+graph = create_tree(db, root_child=30)
+pos = nx.drawing.nx_pydot.pydot_layout(graph, prog='dot')
+nx.draw_networkx(graph, pos = pos, node_size=15, width=0.3, alpha=0.8, node_color="skyblue",edge_color='grey', with_labels=False)
+plt.savefig('propagation_graph.png')
+plt.show()
 
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+# pos = nx.nx_pydot.graphviz_layout(graph, prog="neato")
+# pos = graphviz_layout(graph, prog="dot")#, args="")
+# pos[0] = np.array([0, 0])
+# plt.figure(figsize=(8, 8))
+# nx.draw_networkx(graph, pos = pos, node_size=15, width=0.3, alpha=0.5, node_color="skyblue",edge_color='grey', with_labels=False)
+# plt.axis("equal")
+# plt.show()
+# return
 
+# if __name__ == "__main__":
+#     # execute only if run as a script
+#     main()
