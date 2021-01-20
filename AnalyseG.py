d6b35d83d00574a55b94cbdbb164e6567a6cc2cf
MathurinMaltot <mathurinmat@gmail.com>
2021-01-20 14:27:49 +0100

    AnalyseG

--- AnalyseG.py
+++ AnalyseG.py
@@ -9,7 +9,6 @@ def precision_graphe(db):
     erreur2=0
     c2=0
     for i in range(db.shape[0]):
-        
         erreur_loc2=abs(int(db.at[i,'nretweets'])-len(db.at[i,'Enfants']))
         erreur2+=erreur_loc2
         c2+=1
@@ -18,6 +17,20 @@ def precision_graphe(db):
             erreur+=erreur_loc
             c+=1
     return erreur/c,erreur2/c2
+
+def Lchaine_max(db):
+    Lchaine_max=0
+    indice=-1
+    for i in range(db.shape[0]):
+        Lchaine=0
+        childs=db.at[i,'Enfants'].values[0]
+        
+
+
+        if Lchaine>Lchaine_max:
+            Lchaine_max=Lchaine
+            indice=i
+    return Lchaine_max,indice
 
 def pseudo_main():
     db = pd.read_csv('ForumCarbone_treated.csv',encoding='latin-1')
