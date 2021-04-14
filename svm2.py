5c1ecf3a786a9c6101e0b9282506a7edc2d37d29
ThibautHurson <thibaut.hurson@supelec.fr>
2021-04-14 18:39:23 +0200

    cross val et evaluation

--- svm2.py
+++ svm2.py
@@ -97,7 +97,7 @@ model.fit(X_train, y_train)
 evaluate_model(model)
 
 
-def evaluate_model(model):
+def evaluate_model(model,X_test,y_pred):
     #Predict the response for test dataset
     y_pred = model.predict(X_test)
 
